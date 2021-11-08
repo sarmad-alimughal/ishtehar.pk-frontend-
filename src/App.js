d3f75f09c99dd52959d9d6d51ba81b88e8f07208
sarmad-alimughal <sarmad.ali@invozone.com>
2021-11-08 12:26:35 +0500

    Notices in Tabluar with Material UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Notices from "./components/Notices";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Notices />
+      </header>
+    </div>
+  );
+}
+
+export default App;
